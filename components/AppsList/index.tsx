e14cd21185666f28dbce0414ba3460b38a64a05d
Michael Cheng <michaelnccu@gmail.com>
2023-04-13 23:42:24 +0800

    update reset pswd settings page bg - 3

--- components/AppsList/index.tsx
+++ components/AppsList/index.tsx
@@ -1,6 +1,10 @@
 import Box from "@mui/material/Box"
-import AppItem from "../AppItem"
+import { useSelector } from "react-redux"
 import styled from "styled-components"
+
+import { selectActiveNav } from "../../state/store/slice/layoutSlice"
+import { Navs } from "../../types/enum"
+import AppItem from "../AppItem"
 
 interface AppsListProps {}
 const AppsList: React.FC<AppsListProps> = () => {
@@ -10,48 +14,54 @@ const AppsList: React.FC<AppsListProps> = () => {
     right: 20px;
   `
   const StyledLink = styled.a`
-    font-family: 'Open Sans';
+    font-family: "Open Sans";
     font-size: 14px;
     line-height: 20px;
-    color: #C0C0C0;
+    color: #c0c0c0;
     cursor: pointer;
   `
   const StyledLine = styled.span`
-  display: inline-block;
-  width: 1px;
-  height: 12px;
-  padding-right: 16px;
-  margin-right: 16px;
-  border-right: 1px solid #C0C0C0;
+    display: inline-block;
+    width: 1px;
+    height: 12px;
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid #c0c0c0;
   `
 
+  const activeNav = useSelector(selectActiveNav)
+
   return (
-    <Box
-      mt="200px"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      width="61.2vw"
-      fontFamily="open sans"
-      position="relative"
-    >
-      <Box fontSize="30px" color="#fff">
-        Access Anywhere With Cooler Master ID
-      </Box>
-      <Box fontSize="20px" color="#C0C0C0" mt="6px">
-        One account is all you need. Start your virtual adventure.
-      </Box>
-      <Box display="flex" gap="40px" mt="48px">
-        <AppItem appIcon="MasterControl" appName="Master Control" />
-        <AppItem appIcon="Stormplay" appName="Stormplay" />
-        <AppItem appIcon="Cmodx" appName="CMODX" />
-      </Box>
-      <StyledNav>
-        <StyledLink>Terms of Service</StyledLink>
-        <StyledLine />
-        <StyledLink>Privacy Policy</StyledLink>
-      </StyledNav>
-    </Box>
+    <>
+      {activeNav !== Navs.SETTINGS ? (
+        <Box
+          mt="200px"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          width="61.2vw"
+          fontFamily="open sans"
+          position="relative"
+        >
+          <Box fontSize="30px" color="#fff">
+            Access Anywhere With Cooler Master ID
+          </Box>
+          <Box fontSize="20px" color="#C0C0C0" mt="6px">
+            One account is all you need. Start your virtual adventure.
+          </Box>
+          <Box display="flex" gap="40px" mt="48px">
+            <AppItem appIcon="MasterControl" appName="Master Control" />
+            <AppItem appIcon="Stormplay" appName="Stormplay" />
+            <AppItem appIcon="Cmodx" appName="CMODX" />
+          </Box>
+          <StyledNav>
+            <StyledLink>Terms of Service</StyledLink>
+            <StyledLine />
+            <StyledLink>Privacy Policy</StyledLink>
+          </StyledNav>
+        </Box>
+      ) : null}
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import "@fontsource/teko"
 import Box from "@mui/material/Box"
 import { theme, globalStyles, ThemeProps } from "@ory/themes"
 import type { AppProps } from "next/app"
-import { Provider, useSelector } from "react-redux"
+import { Provider } from "react-redux"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { ThemeProvider } from "styled-components"
@@ -13,16 +13,12 @@ import { createGlobalStyle } from "styled-components"
 import AppsList from "../components/AppsList"
 import PopupLayout from "../components/Layout/PopupLayout"
 import store from "../state/store"
-import { selectActiveNav } from "../state/store/slice/layoutSlice"
-import { Navs } from "../types/enum"
 
 const GlobalStyle = createGlobalStyle((props: ThemeProps) =>
   globalStyles(props),
 )
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // const activeNav = useSelector(selectActiveNav)
-
   return (
     <div data-testid="app-react">
       <Provider store={store}>
