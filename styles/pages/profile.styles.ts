cad9310cc8a767aa23a59c40c6e17898d56dbb15
Kranti <darkphoton20@gmail.com>
2023-04-17 19:52:49 +0800

    WIP, stashing the rest, testing how it looks locally compared to live

---
+++ styles/pages/profile.styles.ts
@@ -0,0 +1,11 @@
+import { styled } from "@mui/system"
+
+const StyledProfileArea = styled("div")(() => ({
+  backgroundColor: "#272735",
+  paddingLeft: "74px",
+  paddingRight: "48px",
+  paddingBottom: "96px",
+  borderRadius: "12px",
+}))
+
+export { StyledProfileArea }
