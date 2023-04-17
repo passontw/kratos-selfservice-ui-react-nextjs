e15a4201f91616433521fd9414bc11fada9ec1b4
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2023-04-17 14:35:48 +0800

    reset password schema 增加密碼的檢核

--- util/schemas.ts
+++ util/schemas.ts
@@ -14,7 +14,18 @@ export const passwordSchema = yup
   .required('password can not be empty.');
 
 export const updatePasswordSchema = yup.object().shape({
-  password: yup.string().required('password can not be empty.'),
+  password: yup
+    .string()
+    .min(8, 'Need at least 8 characters')
+    .matches(
+      /^(?=.*\d)/,
+      'Need at least 1 number'
+    )
+    .matches(
+      /^([a-zA-Z]+)\w*$/,
+      'Need at least 1 alphabet letter'
+    )
+    .required('password can not be empty.'),
   confirmPassword: yup.string().when('password', (password, field) =>
   password ? field.required().oneOf([yup.ref('password')]) : field
 ),
