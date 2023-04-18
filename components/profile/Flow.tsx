039060632a7471f8dccafb78a210237670fce79c
Kranti <darkphoton20@gmail.com>
2023-04-18 16:02:00 +0800

    reverting to check bug

--- components/profile/Flow.tsx
+++ components/profile/Flow.tsx
@@ -1,10 +1,4 @@
 import {
-  StyledForm,
-  StyledImageUpload,
-  StyledProfileDeco,
-  StyledSideInputs,
-} from "../../styles/pages/profile.styles"
-import {
   LoginFlow,
   RecoveryFlow,
   RegistrationFlow,
@@ -189,13 +183,6 @@ export default class Flow<T extends Values> extends Component<
 
     // Filter the nodes - only show the ones we want
     const nodes = this.filterNodes()
-    console.log("@profile nodes:", nodes)
-
-    // acquire profileNode
-    const profileNode = nodes?.find((node) => node.name === "traits.avatar")
-    const profileNodeId = getNodeId(profileNode) as keyof Values
-    // remove it from the other nodes to sperate its view from the rest of the form
-    // nodes.splice(nodes.indexOf(profileNode), 1)
 
     if (!flow) {
       // No flow was set yet? It's probably still loading...
@@ -212,67 +199,36 @@ export default class Flow<T extends Values> extends Component<
         onSubmit={this.handleSubmit}
       >
         {!hideGlobalMessages ? <Messages messages={flow.ui.messages} /> : null}
+        {nodes.map((node, k) => {
+          // console.log(node)
+          const id = getNodeId(node) as keyof Values
+          // if (this.props.noEmail && node.meta.label?.text === "E-Mail") return
+          // if (node.meta.label?.text === "E-Mail") return
 
-        <StyledForm>
-          <StyledImageUpload>
-            {/* {profileNode && (
-              <Node
-                disabled={isLoading}
-                node={profileNode}
-                value={values[profileNodeId]}
-                dispatchSubmit={this.handleSubmit}
-                setValue={(value) =>
-                  new Promise((resolve) => {
-                    this.setState(
-                      (state) => ({
-                        ...state,
-                        values: {
-                          ...state.values,
-                          [getNodeId(profileNode)]: value,
-                        },
-                      }),
-                      resolve,
-                    )
-                  })
-                }
-              />
-            )} */}
-          </StyledImageUpload>
-
-          <StyledSideInputs>
-            <StyledProfileDeco src={"/images/purple-deco.png"} />
-            {nodes.map((node, k) => {
-              // console.log(node)
-              const id = getNodeId(node) as keyof Values
-              // if (this.props.noEmail && node.meta.label?.text === "E-Mail") return
-              // if (node.meta.label?.text === "E-Mail") return
-
-              // return (
-              //   <Node
-              //     key={`${id}-${k}`}
-              //     disabled={isLoading}
-              //     node={node}
-              //     value={values[id]}
-              //     dispatchSubmit={this.handleSubmit}
-              //     setValue={(value) =>
-              //       new Promise((resolve) => {
-              //         this.setState(
-              //           (state) => ({
-              //             ...state,
-              //             values: {
-              //               ...state.values,
-              //               [getNodeId(node)]: value,
-              //             },
-              //           }),
-              //           resolve,
-              //         )
-              //       })
-              //     }
-              //   />
-              // )
-            })}
-          </StyledSideInputs>
-        </StyledForm>
+          return (
+            <Node
+              key={`${id}-${k}`}
+              disabled={isLoading}
+              node={node}
+              value={values[id]}
+              dispatchSubmit={this.handleSubmit}
+              setValue={(value) =>
+                new Promise((resolve) => {
+                  this.setState(
+                    (state) => ({
+                      ...state,
+                      values: {
+                        ...state.values,
+                        [getNodeId(node)]: value,
+                      },
+                    }),
+                    resolve,
+                  )
+                })
+              }
+            />
+          )
+        })}
       </form>
     )
   }
