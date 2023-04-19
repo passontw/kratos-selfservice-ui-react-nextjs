f560238c306ef64102bea9696d93e9e72f4b8683
kiki <kiki.ciou@cloud-interactive.com>
2023-04-19 11:25:57 +0800

    changePassword sumbit margin

--- components/changepassword/Flow.tsx
+++ components/changepassword/Flow.tsx
@@ -265,7 +265,7 @@ export default class Flow<T extends Values> extends Component<
             <Eye setInputType={this.handleEye} />
           </StyledPasswordIcon>
         </StyledDefaultInput>
-        <Box position="relative" width="76px" mt="16px">
+        <Box position="relative" width="76px" mt="24px">
           <NodeInputSubmit
             value={values[getNodeId(submitNode)]}
             node={submitNode}
