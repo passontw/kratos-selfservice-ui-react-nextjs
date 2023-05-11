3bbeb93ff51230f04f6f90177a835edadf925e9c
cvi-tomas-lin <cvi.tomas.lin@gmail.com>
2023-05-11 23:01:49 +0000

    [MC-239] 修正密碼檢查的正規表示是

--- util/schemas.ts
+++ util/schemas.ts
@@ -5,7 +5,7 @@ export const passwordSchema = yup
   .min(8, "Need at least 8 characters")
   .matches(/(?=.*\d)/, "Need at least 1 number")
   .matches(
-    /^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/,
+    /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/,
     "Need at least 1 alphabet letter",
   )
   .required("password can not be empty.")
@@ -20,13 +20,13 @@ export const recoveryFormSchema = yup.object().shape({
 export const changePasswordSchema = yup.object().shape({
   password: yup
     .string()
-    .matches(/^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/, "Need at least 1 alphabet letter")
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/, "Need at least 1 alphabet letter")
     .matches(/(?=.*\d)/, "Need at least 1 number")
     .required("This field is required, please fill it out.")
     .min(8, "Need at least 8 characters"),
   confirmPassword: yup
     .string()
-    .matches(/^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/, "Need at least 1 alphabet letter")
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/, "Need at least 1 alphabet letter")
     .matches(/(?=.*\d)/, "Need at least 1 number")
     .min(8, "Need at least 8 characters")
     .when("password", (password, field) => {
@@ -43,13 +43,13 @@ export const changePasswordSchema = yup.object().shape({
 export const updatePasswordSchema = yup.object().shape({
   password: yup
     .string()
-    .matches(/^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/, "Need at least 1 alphabet letter")
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/, "Need at least 1 alphabet letter")
     .matches(/(?=.*\d)/, "Need at least 1 number")
     .required("Required")
     .min(8, "Need at least 8 characters"),
   confirmPassword: yup
     .string()
-    .matches(/^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/, "Need at least 1 alphabet letter")
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/, "Need at least 1 alphabet letter")
     .matches(/(?=.*\d)/, "Need at least 1 number")
     .min(8, "Need at least 8 characters")
     .when("password", (password, field) => {
@@ -73,7 +73,7 @@ export const loginFormSchema = yup.object().shape({
     .min(8, "Need at least 8 characters")
     .matches(/(?=.*\d)/, "Need at least 1 number")
     .matches(
-      /^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/,
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/,
       "Need at least 1 alphabet letter",
     )
     .required("This field is required, please fill it out."),
@@ -92,7 +92,7 @@ export const registrationFormSchema = yup.object().shape({
     .min(8, "Need at least 8 characters")
     .matches(/(?=.*\d)/, "Need at least 1 number")
     .matches(
-      /^(?=.*[A-Za-z])[A-Za-z$@$!%*#?&]/,
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/,
       "Need at least 1 alphabet letter",
     )
     .required("This field is required, please fill it out."),
