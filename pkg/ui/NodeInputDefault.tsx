89cf1e00f7c2b68fc6715c5abef57c27472c941a
ci-kranti-nebhwani <kranti.nebhwani@cloud-interactive.com>
2023-07-23 20:09:55 +0800

    Adding the fix for resend code wrongfully firing validation

--- pkg/ui/NodeInputDefault.tsx
+++ pkg/ui/NodeInputDefault.tsx
@@ -155,10 +155,10 @@ export function NodeInputDefault<T>(props: NodeInputProps) {
   }, [value])
 
   const accountError =
-  validationMsgs &&
-  (validationMsgs[0]?.text.includes("Email account") ||
-  validationMsgs[0]?.text.includes("The provided credentials are invalid"))
-  
+    validationMsgs &&
+    (validationMsgs[0]?.text.includes("Email account") ||
+      validationMsgs[0]?.text.includes("The provided credentials are invalid"))
+
   const getVerifyCodeConditions = () => {
     if (
       nav === Navs.VERIFICATION &&
@@ -206,6 +206,8 @@ export function NodeInputDefault<T>(props: NodeInputProps) {
       : nav === Navs.VERIFICATION || nav === Navs.RECOVERY
       ? "0 0 38px 0"
       : "unset"
+
+  console.log("@resend test")
 
   return (
     <>
@@ -288,11 +290,11 @@ export function NodeInputDefault<T>(props: NodeInputProps) {
                     let displayText = text
                     if (text.includes("is missing")) {
                       displayText =
-                      "This field is required, please fill it out."
+                        "This field is required, please fill it out."
                     }
                     if (text.includes("is not valid")) {
                       displayText =
-                      "Invalid email format, please check and try again."
+                        "Invalid email format, please check and try again."
                     }
                     return (
                       <span
