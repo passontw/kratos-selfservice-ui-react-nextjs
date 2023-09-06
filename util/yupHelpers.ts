0ef63cd910aa92c81423383722ab59c564979cfc
Cloud.Devops <devops@cloud-interactive.com>
2023-09-06 11:54:45 +0800

    infra 調整

--- util/yupHelpers.ts
+++ util/yupHelpers.ts
@@ -1,10 +1,11 @@
 export const handleYupSchema = async (schema, payload: any) => {
+  console.log("handleYupSchema", JSON.stringify([schema, payload]))
   return schema.validate(payload, { abortEarly: false });
 }
 
 export const handleYupErrors = (errors: unknown) => {
   if(!errors.inner) return errors;
-
+  console.log("handleYupErrors", JSON.stringify(errors))
   return errors.inner.reduce((currentError: any, nextError: { path: any; message: any; }) => {
     const name = nextError.path;
     const message = nextError.message;
